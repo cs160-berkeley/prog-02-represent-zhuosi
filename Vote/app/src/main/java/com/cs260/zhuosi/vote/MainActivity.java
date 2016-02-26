71c8313728e5e63888265a53ef8939e69db956d5
zhuosi <wangzhuosi13@hotmail.com>
2016-02-26 00:39:15 -0800

    in the middle of changing title bar.

--- Vote/app/src/main/java/com/cs260/zhuosi/vote/MainActivity.java
+++ Vote/app/src/main/java/com/cs260/zhuosi/vote/MainActivity.java
@@ -1,13 +1,26 @@
 package com.cs260.zhuosi.vote;
 
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.DrawableRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_main);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.titlebar);
+
+
+//        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#27AE60")));
+//        getSupportActionBar().setIcon(R.mipmap.vote_title);
     }
 }
