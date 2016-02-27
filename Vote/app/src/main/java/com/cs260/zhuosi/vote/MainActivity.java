4d6d18e718d1b9edd20cac1ffd96f8990a579af9
zhuosi <wangzhuosi13@hotmail.com>
2016-02-26 17:43:12 -0800

    header is finally working. Cheers

--- Vote/app/src/main/java/com/cs260/zhuosi/vote/MainActivity.java
+++ Vote/app/src/main/java/com/cs260/zhuosi/vote/MainActivity.java
@@ -1,15 +1,18 @@
 package com.cs260.zhuosi.vote;
 
-import android.app.ActionBar;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.DrawableRes;
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    private Button titleButton = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.titlebar);
 
+
+        titleButton = (Button)this.findViewById(R.id.titleButton);
+        titleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Hola",Toast.LENGTH_SHORT).show();
+            }
+        });
 
 //        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#27AE60")));
 //        getSupportActionBar().setIcon(R.mipmap.vote_title);
