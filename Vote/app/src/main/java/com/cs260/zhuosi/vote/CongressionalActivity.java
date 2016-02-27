80e8fcef2386e933389965abcf4729b81a78542d
zhuosi <wangzhuosi13@hotmail.com>
2016-02-27 00:51:39 -0800

    finished zip code view, congression view and the main view and the connections between them. Tomorrow will aim for finishing all the pages and connections on both the phone and the smart watch. Maybe can do the phone watch connection on Monday night. The best case is to finish tomorrow.

---
+++ Vote/app/src/main/java/com/cs260/zhuosi/vote/CongressionalActivity.java
@@ -0,0 +1,49 @@
+package com.cs260.zhuosi.vote;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+/**
+ * Created by Zhuosi on 2/26/16.
+ */
+public class CongressionalActivity extends Activity {
+    private Button titleButton = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+        setContentView(R.layout.activity_congressional);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.titlebar);
+
+
+        titleButton = (Button)this.findViewById(R.id.titleButton);
+        titleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CongressionalActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
+
+//        TextView textView =(TextView)findViewById(R.id.textView);
+//        textView.setClickable(true);
+//        textView.setMovementMethod(LinkMovementMethod.getInstance());
+//        String text = "<a href='http://www.google.com'> Google </a>";
+//        textView.setText(Html.fromHtml(text));
+
+
+
+    }
+
+}
--- Vote/app/src/main/java/com/cs260/zhuosi/vote/MainActivity.java
+++ Vote/app/src/main/java/com/cs260/zhuosi/vote/MainActivity.java
@@ -1,7 +1,9 @@
 package com.cs260.zhuosi.vote;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.support.annotation.DrawableRes;
 import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
@@ -10,9 +12,20 @@ import android.view.View;
 import android.view.Window;
 import android.widget.*;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends Activity {
 
     private Button titleButton = null;
+    private Button zipButton = null, locationButton = null;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,16 +35,67 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.titlebar);
 
-
-        titleButton = (Button)this.findViewById(R.id.titleButton);
-        titleButton.setOnClickListener(new View.OnClickListener() {
-            @Override
+        zipButton = (Button) this.findViewById(R.id.zipButton);
+        zipButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Hola",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, ZipcodeActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
             }
         });
 
-//        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#27AE60")));
-//        getSupportActionBar().setIcon(R.mipmap.vote_title);
+
+        locationButton = (Button) this.findViewById(R.id.locationButton);
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CongressionalActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.cs260.zhuosi.vote/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.cs260.zhuosi.vote/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
