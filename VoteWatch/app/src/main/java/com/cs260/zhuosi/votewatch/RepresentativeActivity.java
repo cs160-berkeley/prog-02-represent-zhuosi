583aaad46b3e66a6ef8fcdf1df364deb19415e72
zhuosi <wangzhuosi13@hotmail.com>
2016-02-27 22:12:26 -0800

    finished watch layout design

--- VoteWatch/app/src/main/java/com/cs260/zhuosi/votewatch/RepresentativeActivity.java
+++ VoteWatch/app/src/main/java/com/cs260/zhuosi/votewatch/RepresentativeActivity.java
@@ -1,28 +1,33 @@
 package com.cs260.zhuosi.votewatch;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+/**
+ * Created by Zhuosi on 2/27/16.
+ */
+public class RepresentativeActivity extends Activity {
 
-    private TextView mTextView;
+    TextView maneText = null;
+    ImageView repImage = null, partyImage = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_representative);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                maneText = (TextView) stub.findViewById(R.id.nameText);
+                repImage = (ImageView) stub.findViewById(R.id.repImage);
+                partyImage = (ImageView) stub.findViewById(R.id.partyImage);
             }
         });
-        Intent intent = new Intent(MainActivity.this, RepresentativeActivity.class);
-        startActivity(intent);
     }
+
 
 }
