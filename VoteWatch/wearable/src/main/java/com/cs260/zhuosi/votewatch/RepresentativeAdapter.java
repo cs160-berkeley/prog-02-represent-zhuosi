a1b79dfc7013ec56a186d310936f05e45a798281
zhuosi <wangzhuosi13@hotmail.com>
2016-03-01 13:06:00 -0800

    swiping view on the watch

--- VoteWatch/wearable/src/main/java/com/cs260/zhuosi/votewatch/RepresentativeAdapter.java
+++ VoteWatch/wearable/src/main/java/com/cs260/zhuosi/votewatch/RepresentativeAdapter.java
@@ -1,7 +1,10 @@
 package com.cs260.zhuosi.votewatch;
 
 import android.content.Context;
-import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,52 +16,36 @@ import android.widget.Toast;
 /**
  * Created by Zhuosi on 2/29/16.
  */
-public class RepresentativeAdapter extends BaseAdapter {
-    private final Context context;
-    private final int[] pictures;
-    private final int[] party;
-    private final String[] person;
+public class RepresentativeAdapter extends FragmentStatePagerAdapter {
+    String[] NameList;
+    String[] PartyList;
 
-    public RepresentativeAdapter(Context context, int[] pictures, int[] party, String[] person){
-        this.context = context;
-        this.pictures = pictures;
-        this.party = party;
-        this.person = person;
+    public RepresentativeAdapter(FragmentManager fm, String[] name_list, String[] party_list){
+        super(fm);
+        NameList = name_list;
+        PartyList = party_list;
     }
 
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent){
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View representativeView = inflater.inflate(R.layout.activity_representative, parent, false);
-        ImageView pictureView = (ImageView) representativeView.findViewById(R.id.repImage);
-        ImageView partyView = (ImageView) representativeView.findViewById(R.id.partyImage);
-        TextView nameView = (TextView) representativeView.findViewById(R.id.nameText);
+    public Fragment getItem(int position) {
+        Fragment fragment = new RepresentativeFragment();
+        Bundle bundle = new Bundle();
 
-        pictureView.setImageResource(pictures[position]);
-        partyView.setImageResource(party[position]);
-        nameView.setText(person[position]);
+        bundle.putInt("Length", NameList.length);
 
-        representativeView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                Intent intent = new Intent(RepresentativeActivity.class, RepresentativeActivity.class);
-//                startActivity(intent);
-                Toast.makeText(context, person[position], Toast.LENGTH_SHORT).show();
-            }
-        });
+        if(position < NameList.length) {
+            bundle.putString("Name", NameList[position]);
+            bundle.putString("Party", PartyList[position]);
+        }
+        else if(position == NameList.length){
+            bundle.putString("Location", "United States");
+        }
 
-        return representativeView;
-    }
-
-    public long getItemId(int position){
-        return position;
-    }
-
-    public Object getItem(int arg0){
-        return null;
+        fragment.setArguments(bundle);
+        return fragment;
     }
 
     public int getCount(){
-        return person.length;
+        return NameList.length + 1;
     }
 }
